fab53b10ec48526d5a4ba2d2fc969383bdd750cb
joey <flyfish.zy@gmail.com>
2021-07-28 10:15:51 +0800

    remoteEntry.js support output format

--- rollup-remote/plugin/rollup-plugin-federation.js
+++ rollup-remote/plugin/rollup-plugin-federation.js
@@ -1,5 +1,8 @@
 const path = require('path');
 
+const remoteEntryHelperId = 'rollup-plugin-federation/remoteEntry';
+const moduleMapMarker = '__ROLLUP_FEDERATION_MODULE_MAP__';
+
 function moduleMap(exposes, rawFile, distFile) {
     for (let key in exposes) {
         const exposeFile = path.resolve(exposes[key]);
@@ -9,23 +12,8 @@ function moduleMap(exposes, rawFile, distFile) {
     }
     return '';
 }
-
-export default function federation(options) {
-    return {
-        name: 'federation',
-
-        generateBundle(_options, bundle) {
-            let modules = '';
-            for (const file in bundle) {
-                const chunk = bundle[file];
-                if (chunk.type === 'chunk') {
-                    modules += moduleMap(options.exposes, chunk.facadeModuleId, chunk.fileName);
-                }
-            }
-
-            const code = `var moduleMap={
-    ${modules}
-};
+const code =
+    `${moduleMapMarker}
 export const get =(module, getScope) => {
     return moduleMap[module]();
 };
@@ -33,11 +21,48 @@ export const init =(shareScope, initScope) => {
     console.log('init')
 };`
 
+export default function federation(options) {
+    return {
+        name: 'federation',
+
+        buildStart(_options) {
             this.emitFile({
                 fileName: options.filename,
-                type: 'asset',
-                source: code
+                type: 'chunk',
+                id: remoteEntryHelperId
             })
+        },
+
+        resolveId(source, importer) {
+            if (source === remoteEntryHelperId)
+                return source
+            return null;
+        },
+
+        load(id) {
+            if (id === remoteEntryHelperId) {
+                return {
+                    code,
+                    moduleSideEffects: "no-treeshake"
+                }
+            }
+            return null
+        },
+
+        generateBundle(_options, bundle) {
+            let modules = '';
+            let remoteEntry;
+            for (const file in bundle) {
+                const chunk = bundle[file];
+                if (chunk.type === 'chunk') {
+                    modules += moduleMap(options.exposes, chunk.facadeModuleId, chunk.fileName);
+                }
+                if (chunk.fileName === options.filename) {
+                    remoteEntry = chunk;
+                }
+            }
+            remoteEntry.code = remoteEntry.code.replace(moduleMapMarker,
+                `let moduleMap={ ${modules} }`);
         }
     }
 }
